b5155fb5518052ce8f80124fe7a89c05d77372d2
JGMelon22 <jgvasconcellos22@gmail.com>
2026-05-12 16:56:52 -0300

    feat(model): add WeatherResponse data classes for OpenWeatherMap API

---
+++ app/src/main/java/com/example/weatherappkotlin/model/WeatherResponse.kt
@@ -0,0 +1,37 @@
+package com.example.weatherappkotlin.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class WeatherResponse(
+    val name: String,
+    val weather: List<Weather>,
+    val main: MainInfo,
+    val wind: Wind,
+    val sys: Sys
+)
+
+@kotlinx.serialization.Serializable
+data class Weather(
+    val description: String,
+    val icon: String
+)
+
+@Serializable
+data class MainInfo(
+    val temp: Double,
+    @SerialName("feels_like") val feelsLike: Double,
+    val humidity: Int,
+    val pressure: Int
+)
+
+@Serializable
+data class Wind(
+    val speed: Double
+)
+
+@Serializable
+data class Sys(
+    val country: String
+)
